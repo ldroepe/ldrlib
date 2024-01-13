f9435f876d2c485bfea2b830f9c884f61d4df8e1
Luke Roepe <ldroepe@gmail.com>
2024-01-13 12:04:18 -0500

    Initial find commit
    Include find_first_before and find_last_before & include some simple
    tests to go with them. Nothing exhaustive, but it's basic
    verification.

--- include/utils/finds.h
+++ include/utils/finds.h
@@ -2,44 +2,51 @@
 #define _LDRLIB_FINDS_H_
 
 #include <optional>
+#include <iostream>
 
 namespace ldr
 {
 
-template <typename InputIt, typename UnaryPredicate1, typename UnaryPredicate2>
-InputIt find_first_before(InputIt first, InputIt last,
-                          UnaryPredicate1 upper_bound_fn, UnaryPredicate2 selection_fn)
+template <typename I, typename UnaryPredicate1, typename UnaryPredicate2>
+I find_first_before(
+    I first, 
+    I last,
+    UnaryPredicate1 selection_fn, 
+    UnaryPredicate2 upper_bound_fn)
 {
-    std::optional<InputIt> first_before;
+    I first_before = last;
 
-    for( ; first != last; ++first)
+    while(first != last)
     {
-        if(!first_before.has_value() && selection_fn(*first))
-            first_before.emplace(first);
-
+        if(first_before == last && selection_fn(*first))
+            first_before = first;
         if(upper_bound_fn(*first))
-            return first_before.value_or(first);
+            return first_before;
+        ++first;
     }
 
     return last;
 }
 
-template <typename InputIt, typename UnaryPredicate1, typename UnaryPredicate2>
-InputIt find_last_before(InputIt first, InputIt last, 
-                          UnaryPredicate1 upper_bound_fn, UnaryPredicate2 selection_fn)
+template <typename I, typename UnaryPredicate1, typename UnaryPredicate2>
+I find_last_before(
+    I first, 
+    I last, 
+    UnaryPredicate1 selection_fn,
+    UnaryPredicate2 upper_bound_fn)
 {
-    std::optional<InputIt> last_before;
+    I last_before = last;
 
-    for( ; first != last; ++first)
+    while(first != last)
     {
         if(selection_fn(*first))
-            last_before.emplace(first);
-
+            last_before = first;
         if(upper_bound_fn(*first))
-            return last_before.value_or(first);
+            return last_before;
+        ++first;
     }
 
-    return last;
+    return last_before;
 }
 
 } // namespace ldr
--- test/utils/test_finds.cpp
+++ test/utils/test_finds.cpp
@@ -4,50 +4,96 @@
 #include <algorithm>
 #include <gtest/gtest.h>
 
+template <typename I>
+std::string to_string(I first, I last)
+{
+    std::ostringstream oss;
+    oss << '[';
+    while(first != last) oss << *first++ << ',';
+    oss << ']';
+    return oss.str();
+}
+
+template <typename T>
+std::string to_string(std::vector<T> v) { return to_string(std::cbegin(v), std::cend(v)); };
+
+template <typename I, typename T>
+std::string found_element_str(I found, const std::vector<T>& v)
+{
+
+    std::ostringstream oss;
+    oss << "found: " << *found;
+    oss << " in ";
+    oss << to_string(v);
+
+    return oss.str();
+}
+
+namespace fns
+{
+    const auto is_even = [](int x) { return !(x&1); };
+    const auto is_multiple_of = [](int modulus) { 
+        return [m=modulus](int x) { return (x % m == 0); };
+    };
+    const auto is_equal_to = [](int x){
+        return [x=x](int y) { return y == x; };
+    };
+}
+
 class FindsFixture : public ::testing::Test
 {
 
 protected:
     
-    std::vector<int> v;
+    std::vector<int> vi;
 
+    constexpr static int n = 20;
     constexpr static int modulus = 5;
-    constexpr static auto is_even = [](int x) { return !(x&1); };
-    constexpr static auto is_multiple_of = [](int modulus) { 
-        return [&](int x) { return (x % modulus == 0); };
-    };
 
-    template <typename T>
-    std::string to_string(std::vector<T> v)
-    {
-        return to_string(std::cbegin(v), std::cend(v));
-    }
-
-    template <typename I>
-    std::string to_string(I first, I last)
-    {
-        std::ostringstream oss;
-        oss << '[';
-        while(first != last) oss << *first++ << ',';
-        oss << ']';
-        return oss.str();
-    }
-
-    FindsFixture(): v(20) { std::iota(std::begin(v), std::end(v), 1); }
+    FindsFixture(): vi(n) { std::iota(std::begin(vi), std::end(vi), 1); }
 };
 
 TEST_F(FindsFixture, test_find_first_before)
 {
 
-    const auto first_even =
-        ldr::find_first_before(std::cbegin(v), std::cend(v), is_multiple_of(modulus), is_even);
-    EXPECT_EQ(*first_even, 2) << "v is: "  << to_string(v);
+    auto found = 
+        ldr::find_first_before(std::cbegin(vi), std::cend(vi), 
+                               fns::is_even, fns::is_multiple_of(modulus));
+    //std::cout << std::distance(found, std::cend(vi)) << '\n';
+    ASSERT_NE(found, std::cend(vi)) << found_element_str(found, vi);
+    EXPECT_EQ(*found, 2) << "vi is: "  << to_string(vi);
 }
 
 TEST_F(FindsFixture, test_find_last_before)
 {
 
     const auto last_even = 
-        ldr::find_last_before(std::cbegin(v), std::cend(v), is_multiple_of(modulus), is_even);
-    EXPECT_EQ(*last_even, 20);
+        ldr::find_last_before(std::cbegin(vi), std::cend(vi), fns::is_even, fns::is_multiple_of(modulus));
+
+    std::cout << "working with: " << to_string(vi) << '\n';
+    ASSERT_NE(last_even, std::cend(vi)) << "dist: " << std::distance(std::cend(vi), last_even);
+    const int check_for = []() {
+        if(modulus%2 == 0)
+            return modulus;
+        else
+            return modulus-1;
+    }();
+    EXPECT_EQ(*last_even, check_for);
+}
+
+TEST_F(FindsFixture, test_fail_to_find)
+{
+    
+    const auto equal_to = [](int x) { return x == n+1; };
+    EXPECT_EQ(std::cend(vi),
+              ldr::find_first_before(std::cbegin(vi), std::cend(vi), 
+                                     equal_to, fns::is_multiple_of(10)));
+}
+
+TEST_F(FindsFixture, test_fail_to_bound)
+{
+    
+    const auto equal_to = [](int x) { return x == n+1; };
+    auto found = ldr::find_first_before(std::cbegin(vi), std::cend(vi), fns::is_even, equal_to);
+    EXPECT_EQ(std::cend(vi), found) << found_element_str(found, vi);
 }
